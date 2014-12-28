9286855632116baea14b9623c0b478a3ee49d5e4
Sebastiaan van Doorn <snvandoorn@googlemail.com>
2014-12-28 19:42:06 +0100

    Split received commands in fob and button id.

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -13,6 +13,34 @@
      
 RCSwitch mySwitch;
  
+void byte_to_str(char *str,int x)
+{
+   int n;
+   for(n=0; n<8;n++ )
+   {
+      if((x & 0x80) !=0)
+      {
+         str[n] = '1';
+      }
+      else
+      {
+         str[n] = '0';
+      }
+    //  if(n==3)
+    //     str[n] = ' '; // space between nybbles
+         
+      x = x<<1;
+   }
+   str[n] = '\0';
+}
+void int16_to_str(char* str,int x)
+{
+   int hi, lo;
+   hi=(x>>8) & 0xff;
+   lo=x&0xff;
+   byte_to_str(str,hi);
+   byte_to_str(&str[9],lo);
+}
 
 
 int main(int argc, char *argv[]) {
@@ -20,7 +48,7 @@ int main(int argc, char *argv[]) {
      // This pin is not the first pin on the RPi GPIO header!
      // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      // for more information.
-     int PIN = 2;
+     int PIN = 15 ;
      
      if(wiringPiSetup() == -1)
        return 0;
@@ -28,7 +56,9 @@ int main(int argc, char *argv[]) {
      mySwitch = RCSwitch();
      mySwitch.enableReceive(PIN);  // Receiver on inerrupt 0 => that is pin #2
      
-    
+ //    char str[18];
+     int fob_id, button_id;
+
      while(1) {
   
       if (mySwitch.available()) {
@@ -38,8 +68,11 @@ int main(int argc, char *argv[]) {
         if (value == 0) {
           printf("Unknown encoding");
         } else {    
-   
-          printf("Received %i\n", mySwitch.getReceivedValue() );
+          fob_id = (value >> 4);
+          button_id=value&0x0f;
+ //        int16_to_str(str,value);
+ //         value = mySwitch.getReceivedValue();
+          printf("%i %i\n", fob_id,button_id);
         }
     
         mySwitch.resetAvailable();
